c0ec40be980e19222c29a7350480bf1791d61ec9
Yevhenii <frank.cadillac92@gmail.com>
2021-06-14 12:00:00 +0300

    Create Kubernetes manifest via Terraform

--- main.tf
+++ main.tf
@@ -1,13 +1,54 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "devtools"
-  machine_type = "f1-micro"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google"
+  project_id             = var.gcp_project
+  name                   = var.gke_cluster_name
+  region                 = var.gcp_region
+  network                = var.gke_network
+  subnetwork             = var.gke_subnetwork
+  ip_range_pods          = "us-west1-01-gke-01-pods"
+  ip_range_services      = "us-west1-01-gke-01-services"
+  create_service_account = false
+  service_account        = var.gke_service_account
+}
+resource "kubernetes_pod" "nginx-example" {
+  metadata {
+    name = "nginx-example"
+
+    labels = {
+      maintained_by = "terraform"
+      app           = "nginx-example"
     }
   }
-  network_interface {
-    network = "default"
-    access_config {}
+
+  spec {
+    container {
+      image = "nginx:1.7.9"
+      name  = "nginx-example"
+    }
   }
+
+  depends_on = [module.gke]
+}
+
+resource "kubernetes_service" "nginx-example" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_pod.nginx-example.metadata[0].labels.app
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 8080
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+
+  depends_on = [module.gke]
 }
--- variables.tf
+++ variables.tf
@@ -23,3 +23,33 @@ variable "gcp_zone" {
   type        = string
   default     = "us-west1-a"
 }
+
+variable "gke_zones" {
+  description = "GKE Zones"
+  type        = list(string)
+  default     = ["us-west1-a,us-west1-b"]
+}
+
+variable "gke_cluster_name" {
+  description = "GKE Cluster Name"
+  type        = string
+  default     = "app-cluster"
+}
+
+variable "gke_network" {
+  description = "GKE Network Name"
+  type        = string
+  default     = "default"
+}
+
+variable "gke_subnetwork" {
+  description = "GKE subnetwork Name"
+  type        = string
+  default     = "us-west1"
+}
+
+variable "gke_service_account" {
+  description = "DKE Service Account"
+  type        = string
+  default     = "devops-java-project@devops-java-project.iam.gserviceaccount.com"
+}
